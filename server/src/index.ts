6a94e7704062ce13ecf33688b2ef8458e6b8ea57
Madhan <mmadhankumar519@gmail.com>
2025-07-23 20:47:00 +0530

    add homeSpace channel for selfTalkIO from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ selfTalkIONameSpace.on('connection', (socket) => {
 });
 
 setInterval(() => {
-  selfTalk.emit('receive_message', `active client's count ${socketIO.engine.clientsCount}`);
+  selfTalkIONameSpace.emit('receive_message', `active client's count ${socketIO.engine.clientsCount}`);
 }, 10000);
 
 
